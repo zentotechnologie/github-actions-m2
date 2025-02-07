62917f37b8c4eafbccd01e7cc17d52b1c422d7fb
AtouiRamzi <128624542+AtouiRamzi@users.noreply.github.com>
2025-02-07 15:04:11 +0100

    Update entrypoint.sh

--- magento-coding-standard/entrypoint.sh
+++ magento-coding-standard/entrypoint.sh
@@ -11,20 +11,24 @@ test -z "${PHPCS_STANDARD}" && PHPCS_STANDARD=$INPUT_PHPCS_STANDARD
 test -z "${PHPCS_SEVERITY}" && PHPCS_SEVERITY=$INPUT_PHPCS_SEVERITY
 test -z "${PHPCS_REPORT}" && PHPCS_REPORT=$INPUT_PHPCS_REPORT
 test -z "${PHPCS_EXTENSIONS}" && PHPCS_EXTENSIONS=$INPUT_PHPCS_EXTENSIONS
+test -z "${PHPCS_IGNORE}" && PHPCS_IGNORE=$INPUT_PHPCS_IGNORE
 
 test -z "${PHPCS_STANDARD}" && PHPCS_STANDARD=Magento2
 test -z "${PHPCS_SEVERITY}" && PHPCS_SEVERITY=8
 test -z "${PHPCS_REPORT}" && PHPCS_REPORT=checkstyle
 test -z "${PHPCS_EXTENSIONS}" && PHPCS_EXTENSIONS=php
+test -z "${PHPCS_IGNORE}" && PHPCS_IGNORE=
 
 echo "PHPCS report: ${PHPCS_REPORT}"
 echo "PHPCS standard: ${PHPCS_STANDARD}"
 echo "PHPCS severity: ${PHPCS_SEVERITY}"
-echo "PHPCS severity: ${PHPCS_EXTENSIONS}"
+echo "PHPCS extentions: ${PHPCS_EXTENSIONS}"
+echo "PHPCS ignore: ${PHPCS_IGNORE}"
 
 sh -c "/root/.composer/vendor/bin/phpcs \
   --report=${PHPCS_REPORT} \
   --extensions=${PHPCS_EXTENSIONS} \
   --severity=${PHPCS_SEVERITY} \
+  --ignore=${PHPCS_IGNORE} \
   --standard=$PHPCS_STANDARD $GITHUB_WORKSPACE \
   -s $*"
